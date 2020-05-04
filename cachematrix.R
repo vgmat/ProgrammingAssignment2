18126fd051c98d6a45ab2c3fc7d1ba9904264d3a
GNANARAJ <50624587+vgmat@users.noreply.github.com>
2020-05-04 20:23:16 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+setwd("J:/COURSERA MATERIALS/R Programming")
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
+#The following function calculates the mean of the special "vector" 
+#created with the above function.
 
-
-## Write a short comment describing this function
-
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+## I Assume the input x as a matrix
+## and then set the solved value "soln" as a null
+## then I changed every reference to "mean" to "solve"
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  soln <- NULL
+  set <- function(y) {
+    x <<- y
+    soln <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) soln <<- solve
+  getsolve <- function() soln
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+##
+## Here I changed "mean" to "solve" and "m" to "soln"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  soln <- x$getsolve()
+  if(!is.null(soln)) {
+    message("getting inversed matrix")
+    return(soln)
+  }
+  data <- x$get()
+  soln <- solve(data, ...)
+  x$setsolve(soln)
+  soln
 }
